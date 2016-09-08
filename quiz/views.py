79fd0cc1896e420632d96e285e413fdba76bf2ae
majaellinorlind <maja.ellinor@gmail.com>
2016-09-08 10:47:57 +0200

    changes name on views to match html files

--- quiz/views.py
+++ quiz/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
-def startpage(request):
-return render(request, "quiz/startpage.html")
+def start(request):
+return render(request, "quiz/start.html")
 
 def quiz(request):
 return render(request, "quiz/quiz.html")
@@ -9,7 +9,7 @@ return render(request, "quiz/quiz.html")
 def question(request):
 return render(request, "quiz/question.html")
 
-def completed(request):
-return render(request, "quiz/completed.html")
+def results(request):
+return render(request, "quiz/results.html")
 
 
