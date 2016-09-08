bf7635849a0e4d878b6597213243148e4ebc4d1d
majaellinorlind <maja.ellinor@gmail.com>
2016-09-08 11:02:27 +0200

    imported test data in views

--- quiz/views.py
+++ quiz/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
 
+quizzes = [
+	{
+		"quiz_number": 1,
+   		"name": "Klassiska böcker",
+	   	"description": "Hur bra kan du dina klassiker?"
+	},
+	{
+		"quiz_number": 2,
+   	   	"name": "Största 1slagen",
+	   	"description": "Kan du dina lag?"
+	},
+	{
+		"quiz_number": 3,
+   	    	"name": "Världens mest kända hackare",
+	    	"description": "Hackerhistoria är viktigt, kan du den?"	},
+]
+
 def start(request):
 	return render(request, "quiz/start.html")
 
