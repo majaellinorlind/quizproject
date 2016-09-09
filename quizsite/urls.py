37e5d1c47624a4218ec9bc77355be386c3a18f14
majaellinorlind <maja.ellinor@gmail.com>
2016-09-09 10:06:13 +0200

    added url patterns for django to import admin url

--- quizsite/urls.py
+++ quizsite/urls.py
@@ -13,11 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.contrib import admin
+from django.conf.urls import include
+
 from django.conf.urls import url
 from quiz import views
 urlpatterns = [
 	url(r"^$", views.start, name="start_page"),
 	url(r"^quiz/([0-9]+)/$", views.quiz, name="quiz_page"),
 	url(r"^quiz/([0-9]+)/question/([0-9]+)/$", views.question, name="question_page"),
-	url(r"^quiz/([0-9]+)/results/$", views.results, name="results_page")
+	url(r"^quiz/([0-9]+)/results/$", views.results, name="results_page"),
+    url(r'^admin/', include(admin.site.urls)),
 	]
